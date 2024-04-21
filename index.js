2b62dde20edfb829ddca089ca88fd4c1d492e781
Dave Banwell <dave@davebanwell.com>
2024-04-21 14:04:22 -0400

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = (drivers) => [drivers[0], drivers[1]]
+
+const returnLastTwoDrivers = (drivers) => [drivers[drivers.length - 2], drivers[drivers.length - 1]]
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = (multiplier) => {
+    const fareMultiplier = (fare) => fare * multiplier;
+    return fareMultiplier;
+  }
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = (drivers, picker) => picker(drivers);
